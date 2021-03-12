daa3a86f3028bc7219647199bda79f3aefd6347f
daviduribe0007 <daviduribe0007@gmail.com>
2021-03-12 10:29:29 -0500

    Creacion del app No me mientas

---
+++ src/main/java/co/com/softka/softkau/Nomemientasapp/NoMeMientasAppApplication.java
@@ -0,0 +1,13 @@
+package co.com.softka.softkau.Nomemientasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMeMientasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoMeMientasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/softka/softkau/Nomemientasapp/NoMeMientasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.softka.softkau.Nomemientasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMeMientasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
