2f231eb9c7e47a7c9e45c3cae43f54fb1579c519
daviduribe0007 <daviduribe0007@gmail.com>
2021-03-14 14:35:53 -0500

    se Crea test del caso de uso CreateRound

---
+++ src/test/java/co/com/softka/softkau/nomemientasapp/usecase/CreateRoundUseCaseTest.java
@@ -0,0 +1,33 @@
+package co.com.softka.softkau.nomemientasapp.usecase;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.support.TriggeredEvent;
+import co.com.softka.softkau.nomemientasapp.domain.game.events.GameStarted;
+import co.com.softka.softkau.nomemientasapp.domain.game.values.identities.PlayerId;
+import co.com.softka.softkau.nomemientasapp.domain.round.events.RoundCreated;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateRoundUseCaseTest {
+    @Test
+    void crearRonda() {
+        var event = new GameStarted(Set.of(PlayerId.of("xxx"), PlayerId.of("fff")));
+        event.setAggregateRootId("222");
+        var useCase = new CreateRoundUseCase();
+
+        var events = UseCaseHandler
+                .getInstance()
+                .syncExecutor(useCase, new TriggeredEvent<>(event))
+                .orElseThrow()
+                .getDomainEvents();
+
+        var rondaCreada = (RoundCreated) events.get(0);
+        Assertions.assertEquals("222", rondaCreada.getJuegoId().value());
+        Assertions.assertEquals(2, rondaCreada.playerIds().size());
+    }
+
+}
